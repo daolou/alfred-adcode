a8b82c23daa195a5004752dd97d6d38bad93b908
叨喽 <zhiguo.jzg@alibaba-inc.com>
2022-04-30 14:28:40 +0800

    fix: 🐛 ERR_MODULE_NOT_FOUND: node esm require extension .js and use cjs

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@
  * @Author: zhiguo.jzg
  * @Date: 2022-04-29 21:00:30
  * @Description: TODO: Description of file, its uses and information
- * @LastEditTime: 2022-04-30 11:43:51
+ * @LastEditTime: 2022-04-30 14:23:22
  * @LastEditors: zhiguo.jzg
  */
-import { run } from './exec';
+import { run } from './exec.js'; // node esm require extension .js
 
 run();
