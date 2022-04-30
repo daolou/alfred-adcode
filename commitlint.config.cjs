3500e14c2f047b0bcbea100e466016c80b692184
KhaZix <jiangzhiguo2010@qq.com>
2022-05-01 07:55:09 +0800

    feat: ✨ optimize Drastically reduced size

---
+++ commitlint.config.cjs
@@ -0,0 +1,12 @@
+/*
+ * @Author: zhiguo.jzg
+ * @Date: 2022-04-01 02:24:16
+ * @Description: TODO: Description of file, its uses and information
+ * @LastEditTime: 2022-05-01 05:44:53
+ * @LastEditors: zhiguo.jzg
+ */
+
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+	rules: {},
+};
